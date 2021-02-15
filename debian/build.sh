455c203a727c7034b8baa9690e3722148c536684
Polygaev <a.polygaev@gmail.com>
2021-02-15 15:12:35 +0300

    build.sh: Added a bash script to call simple-cdd

---
+++ debian/build.sh
@@ -0,0 +1,40 @@
+# $@ - additional simple-cdd parameters
+#
+# Use export SIMPLE_CDD_DISK_LABEL="label" to set CD label
+
+# Please install following packages:
+#   - simple-cdd
+#   - sed
+#   - grep
+
+# Exit on error
+set -e
+
+# Global variables
+SCRIPT_DIR="$(dirname $(readlink -f $0))"
+
+# Export additional global variables
+if [ -f "${SCRIPT_DIR}/export" ]
+then
+    source "${SCRIPT_DIR}/export"
+fi
+
+# Call simple-cdd to create debian CD
+set +e
+    build-simple-cdd \
+	--dist ${DEBIAN_RELEASE} \
+	--conf ./simple-cdd.conf \
+	--dvd \
+	--verbose \
+	$@
+    ret=$?
+set -e
+
+if [ ${ret} -ne 0 ]
+then
+    echo "build-simple-cdd failed"
+    exit 1
+fi
+
+echo "$0 completed"
+exit 0
